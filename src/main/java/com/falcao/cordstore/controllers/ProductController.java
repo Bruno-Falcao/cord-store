4c4bc1e13a3b885375b07e6f8021a3c79922cb27
Bruno-Falcao <brunofalcao321@gmail.com>
2023-02-03 17:34:26 -0300

    Added Pre Authorizations to Product APIs

--- src/main/java/com/falcao/cordstore/controllers/ProductController.java
+++ src/main/java/com/falcao/cordstore/controllers/ProductController.java
@@ -24,6 +24,7 @@ public class ProductController {
     }
 
     @PostMapping("/save_product")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> saveProduct(@RequestBody Product peripheral) {
         try {
             return ResponseEntity.ok()
@@ -37,6 +38,7 @@ public class ProductController {
     }
 
     @PutMapping("/update_product")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> uptdateProduct(@RequestBody Product peripheral) {
         try {
             return ResponseEntity.ok()
@@ -65,6 +67,7 @@ public class ProductController {
     }
 
     @GetMapping("/find_product")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> findProductById(@RequestParam(name = "id") String id) {
         try {
             return ResponseEntity.ok()
@@ -78,6 +81,7 @@ public class ProductController {
     }
 
     @GetMapping("/find_by_visibility")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> findProductByVisibility() {
         try {
             return ResponseEntity.ok()
@@ -91,6 +95,7 @@ public class ProductController {
     }
 
     @GetMapping("/find_by_brand")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> findProductByBrand(@RequestParam(name = "brand") String brand) {
         try {
             return ResponseEntity.ok()
@@ -104,6 +109,7 @@ public class ProductController {
     }
 
     @GetMapping("/find_by_name")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> findProductByName(@RequestParam(name = "name") String name) {
         try {
             return ResponseEntity.ok()
@@ -117,6 +123,7 @@ public class ProductController {
     }
 
     @GetMapping("/find_by_category")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> findProductByCategories(@RequestParam(name = "category") int category) {
         try {
             return ResponseEntity.ok()
@@ -130,6 +137,7 @@ public class ProductController {
     }
 
     @PatchMapping("/delete_product")
+    @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Object> deleteProduct(@RequestParam(name = "id") String id) {
         try {
             return ResponseEntity.ok()
